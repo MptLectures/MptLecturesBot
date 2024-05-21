4946fb41a33644af3d70d714e891a4474f77f5ce
Thy'reNotPineapples95 <ananasovii.yashik@gmail.com>
2024-05-22 00:08:18 +0300

    some fix

--- testing.js
+++ testing.js
@@ -1,30 +1,82 @@
-const { Bot, session  } = require("grammy");
-const { conversations, createConversation } = require("@grammyjs/conversations");
+const { helloKb } = require('./keyboards.js')
+const { OpenAI } = require('openai') 
 require('dotenv').config()
+const { generateUpdateMiddleware } = require("telegraf-middleware-console-time") 
+const { Bot, InlineKeyboard, session, webhookCallback } = require('grammy')
+const { conversations, createConversation } = require("@grammyjs/conversations")
+const fs = require("fs")
+const express = require('express')
 
-const bot = new Bot(process.env.MPTLECTUTES);
+const bot = new Bot(process.env.MPTLECTUTES)
 
-async function greeting(conversation, ctx) {
-    await ctx.reply("Hi there! What is your name?");
-    const { message } = await conversation.wait();
-    await ctx.reply(`Welcome to the chat, ${message.text}!`);
+const app = express()
+app.use(express.json())
+app.use(webhookCallback(bot, 'express'))
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI,
+})
+
+async function GenerateTargetOrTotal(conversation, ctx) {
+    await ctx.reply("Введите ваше ТЗ:")
+    const { message } = await conversation.wait()
+    await ctx.reply(`Создаю цель и вывод исходя из:\n${message.text}!`)
+    const answer = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: message }],
+    });
+    await ctx.reply(answer.choices[0])
 }
 
-// Install the session plugin.
 bot.use(session({
     initial() {
-      // return empty object for now
       return {};
     },
 }));
   
-  // Install the conversations plugin.
 bot.use(conversations());
+bot.use(createConversation(GenerateTargetOrTotal));
+bot.use(generateUpdateMiddleware())
 
-bot.use(createConversation(greeting));
+bot.api.setMyCommands([
+    {
+        command: "start", description: "Начальная команда",
+    },
+    {
+        command: "queue_start", description: "Запустить очередь",
+    },
+    {
+        command: "create_target_and_total", description: "Сгенерировать цель и вывод для практической работы"
+    }
+])
 
-bot.command("start", async (ctx) => {
-    await ctx.conversation.enter("greeting");
+bot.command("create_target_and_total", async (ctx) => {
+    await ctx.conversation.enter("GenerateTargetOrTotal");
 });
 
-bot.start();
+bot.command('start', async (ctx) => {
+    await ctx.reply("Привет!❤️\nЭто бот в котором будут собраны лекции 2 курса📚, а так он способен создавать очереди, на сдачу каких либо работ!)", { reply_markup: helloKb })
+})
+
+bot.command("queue_start", async (ctx) => {
+    await ctx.reply("ahahh")
+})
+
+bot.callbackQuery('q#join', async (ctx) => {
+    await ctx.answerCallbackQuery({
+        text: "Вы вступили в очередь",
+    })
+
+    const queueKeyboard = new InlineKeyboard()
+    .text("Текущая очередь: \n", "q#join")
+
+    const newText = `${ctx.callbackQuery.message.text}\n${ctx.callbackQuery.message.text.split('\n').length}. @${ctx.callbackQuery.message.chat.username}`
+
+    await ctx.editMessageText(newText, {
+        reply_markup: queueKeyboard,
+    })
+
+    console.log(ctx.callbackQuery)
+})
+
+bot.start()
